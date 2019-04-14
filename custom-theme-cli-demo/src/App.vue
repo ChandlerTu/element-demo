e1b740b61c218bcad4b13d6307ac2334291f90a3
Chandler Tu <chandlertu@chandlertu.com>
2019-04-14 16:07:48 +0800

    安装 element

--- custom-theme-cli-demo/src/App.vue
+++ custom-theme-cli-demo/src/App.vue
@@ -2,6 +2,33 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <el-table :data="tableData">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="140"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="120"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址"
+      >
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -12,6 +39,16 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data() {
+    const item = {
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄"
+    };
+    return {
+      tableData: Array(20).fill(item)
+    };
   }
 }
 </script>
