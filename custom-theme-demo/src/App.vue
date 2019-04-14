943c1757f33ca976aa833260c61f281e65eec773
Chandler Tu <chandlertu@chandlertu.com>
2019-04-14 15:55:27 +0800

    引用自定义主题，修改表格背景、字体、鼠标悬停颜色。

--- custom-theme-demo/src/App.vue
+++ custom-theme-demo/src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <el-table :data="tableData">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="140"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="120"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址"
+      >
+      </el-table-column>
+    </el-table>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data() {
+    const item = {
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄"
+    };
+    return {
+      tableData: Array(20).fill(item)
+    };
   }
-}
+};
 </script>
 
 <style>
+body {
+  background: #0f448e;
+  color: #ffffff;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.el-table th {
+  background: #0f448e;
+  color: #ffffff;
+}
+
+.el-table tr {
+  background: #0f448e;
+  color: #ffffff;
+}
+
+.el-table--enable-row-hover .el-table__body tr:hover > td {
+  background: #3f69a5;
 }
 </style>
