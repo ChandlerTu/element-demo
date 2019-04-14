997f868d5929b06096c106bf0a8bf33513b6b5d6
Chandler Tu <chandlertu@chandlertu.com>
2019-04-14 10:26:21 +0800

    增加 el-scrollbar 基础使用方法演示

--- el-scrollbar-demo/src/App.vue
+++ el-scrollbar-demo/src/App.vue
@@ -1,29 +1,31 @@
 <template>
   <div id="app">
-    <img
-      alt="Vue logo"
-      src="./assets/logo.png"
-    >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <el-table :data="tableData">
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="140"
+    <el-scrollbar>
+      <img
+        alt="Vue logo"
+        src="./assets/logo.png"
       >
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="120"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址"
-      >
-      </el-table-column>
-    </el-table>
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+      <el-table :data="tableData">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="140"
+        >
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="120"
+        >
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址"
+        >
+        </el-table-column>
+      </el-table>
+    </el-scrollbar>
   </div>
 </template>
 
@@ -49,12 +51,34 @@ export default {
 </script>
 
 <style>
+html {
+  background: red;
+  height: 100%;
+}
+
+body {
+  background: yellow;
+  height: 100%;
+  margin: 0;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: blue;
+  height: 100%;
+}
+
+/* 需要设置 el-scrollbar 的高度，具体数值或100%，使用100%时父层高度要固定。 */
+.el-scrollbar {
+  height: 100%;
+}
+
+/* 隐藏横向滚动条 */
+.el-scrollbar__wrap {
+  overflow-x: hidden;
 }
 </style>
